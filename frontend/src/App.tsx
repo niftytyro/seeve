ebee7acea4698cb45596151f8e24c592b47e287a
Udasi Tharani <udasitharani@gmail.com>
2021-05-14 22:58:30 +0530

    ✨ connect to api to check login status

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Sidebar from "./components/sidebar/sidebar";
 import Login from "./pages/login";
 import Tasks from "./pages/tasks";
-import { sidebarSectionsList } from "./utils";
+import { API_URL, sidebarSectionsList } from "./utils";
 
 const App: React.FC = () => {
   const [selected, setSelected] = useState<number>(0);
-  if (document.cookie.slice(0, 4) !== "jwt=") return <Login />;
+  const [authenticated, setAuthenticated] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(`${API_URL}/auth/me`, {
+          credentials: "include",
+        });
+        if (res.status === 200) setAuthenticated(true);
+      } catch (e) {
+        setAuthenticated(false);
+      }
+    })();
+  }, []);
+
+  if (authenticated === null) return <div />;
+
+  if (!authenticated) return <Login />;
 
   return (
     <div className="flex flex-row p-4 h-full divide-x divide-light-blue-400">
